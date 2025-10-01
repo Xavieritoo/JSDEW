05633d7810500d74e8a88f04fe1389630d5f0490
Javier <javitocc04jc@gmail.com>
2025-10-01 15:05:08 +0100

    Para seguir en clase

--- UT2/AA_21/app.js
+++ UT2/AA_21/app.js
@@ -1,4 +1,16 @@
 let Numeroempleados = 5;
+let empleados =new Array(); 
 for(i=0;i<Numeroempleados;i++){
-    let nombreEmpleados = prompt("Introduce el nombre de esos "+Numeroempleados+" empleados");
+    let nombreEmpleado = prompt("Introduce el nombre de esos "+Numeroempleados+" empleados");
+    empleados.push(nombreEmpleado);
+    let horas = prompt("Introduce las horas que trabajó "+nombreEmpleado+" Esta semana");
+    let pagoHora = prompt("Introduce el pago por hora del empleado "+nombreEmpleado);
 }
+let sueldoBruto;
+let horasExtra = (horas - 40) * pagoHora * 1.5;
+if (horas <= 40){
+    sueldoBruto = horas * pagoHora;
+} else if(horas > 40){
+    sueldoBruto = (pagoHora * 40) + horasExtra;
+}
+let impuesto;
