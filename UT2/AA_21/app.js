90373276cb446304a6d17aabb29c25d6a697480b
Javier <javitocc04jc@gmail.com>
2025-10-01 16:56:37 +0100

    Seguir haciendo en casa

--- UT2/AA_21/app.js
+++ UT2/AA_21/app.js
@@ -1,16 +1,34 @@
-let Numeroempleados = 5;
-let empleados =new Array(); 
-for(i=0;i<Numeroempleados;i++){
-    let nombreEmpleado = prompt("Introduce el nombre de esos "+Numeroempleados+" empleados");
+let Numeroempleados = 2;
+let empleados = new Array();
+let sueldosBruto = new Array();
+let sueldosNetos = new Array();
+for (i = 0; i < Numeroempleados; i++) {
+    let nombreEmpleado = prompt("Introduce el nombre de esos " + Numeroempleados + " empleados");
+    let horas = parseInt(prompt("Introduce las horas que trabajó " + nombreEmpleado + " Esta semana"));
+    let pagoHora = parseFloat(prompt("Introduce el pago por hora del empleado " + nombreEmpleado));
+
+    let sueldoBruto;
+    let horasExtra = (horas - 40) * pagoHora * 1.5;
+    if (horas <= 40) {
+        sueldoBruto = horas * pagoHora;
+    } else if (horas > 40) {
+        sueldoBruto = (pagoHora * 40) + horasExtra;
+    }
+
+    let impuesto;
+    if (sueldoBruto > 1000) {
+        impuesto = sueldoBruto * 0.10;
+    } else {
+        impuesto = sueldoBruto * 0.05;
+    }
+
+    let sueldoNeto = sueldoBruto - impuesto;
+
     empleados.push(nombreEmpleado);
-    let horas = prompt("Introduce las horas que trabajó "+nombreEmpleado+" Esta semana");
-    let pagoHora = prompt("Introduce el pago por hora del empleado "+nombreEmpleado);
+    sueldosBruto.push(sueldoBruto);
+    sueldosNetos.push(sueldoNeto);
+
 }
-let sueldoBruto;
-let horasExtra = (horas - 40) * pagoHora * 1.5;
-if (horas <= 40){
-    sueldoBruto = horas * pagoHora;
-} else if(horas > 40){
-    sueldoBruto = (pagoHora * 40) + horasExtra;
-}
-let impuesto;
+console.log(empleados);
+console.log(sueldosBruto);
+console.log(sueldosNetos); 
