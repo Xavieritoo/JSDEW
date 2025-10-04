33cb450026bb006384e962eac8424d1fee82ad87
Javier <javitocc04jc@gmail.com>
2025-10-04 20:20:20 +0100

    Seguir luego

--- UT2/AA_21/app.js
+++ UT2/AA_21/app.js
@@ -1,19 +1,24 @@
+// Elegir el número de empleados a crear
 let numeroEmpleados = 4;
 let empleados = new Array();
 let sueldosBruto = new Array();
 let sueldosNetos = new Array();
+// Bucle de los empleados a crear
 for (i = 0; i < numeroEmpleados; i++) {
     let nombreEmpleado = prompt("Introduce el nombre del " + (i+1) + "º empleado");
-
+    // Tira error si empleado está vacío si no pushea el empleado al array
     if (!nombreEmpleado) {
         throw new Error('Name Error');
     } else {
         empleados.push(nombreEmpleado);
+        // Las horas para el cálculo del sueldo
         let horas = parseInt(prompt("Introduce las horas que trabajó " + nombreEmpleado + " Esta semana"));
         let pagoHora = parseFloat(prompt("Introduce el pago por hora del empleado " + nombreEmpleado));
+        // Si las horas están vacías o no son un número tira error
         if (isNaN(horas) || !horas || isNaN(pagoHora) || !pagoHora) {
             throw new Error('Number Error or null');
         } else {
+            // Si no, hace el cálculo del sueldo y las mete en el array
             let sueldoBruto;
             let horasExtra = (horas - 40) * pagoHora * 1.5;
             if (horas <= 40) {
@@ -22,7 +27,7 @@ for (i = 0; i < numeroEmpleados; i++) {
                 sueldoBruto = (pagoHora * 40) + horasExtra;
             }
             sueldosBruto.push(sueldoBruto);
-
+            // El cálculo del impuesto para el sueldo neto
             let impuesto;
             if (sueldoBruto > 1000) {
                 impuesto = sueldoBruto * 0.10;
@@ -40,20 +45,22 @@ for (i = 0; i < numeroEmpleados; i++) {
         console.log(sueldosNetos);
     }
 }
-
+// Eliminar el empleado y sus relaciones según su nombre
 let eliminarEmpleado = prompt("Introduce el nombre del empleado que quiera eliminar, en caso contrario déjelo vacío: ");
 let incluye = empleados.indexOf(eliminarEmpleado);
+// Si no existe el empleado, incluye tendrá el valor de -1. Por tanto, en caso inverso lo eliminará
 if (incluye !== -1) {
     delete empleados[incluye];
     delete sueldosBruto[incluye];
     delete sueldosNetos[incluye];
     console.log("Empleado: " + eliminarEmpleado + " se ha eliminado correctamente")
+    // Si no quiere eliminar ninguno no se pondrá nada
 } else if (eliminarEmpleado == "") {
     console.log("No quiere eliminar ningún empleado");
 } else {
     console.log("Empleado no encontrado")
 }
-
+// Mostrar las lista final de los empleados
 for (i = 0; i < empleados.length; i++) {
     console.log("Empleado: " + empleados[i] + " Sueldo Bruto: " + sueldosBruto[i] + " Sueldo Neto: " + sueldosNetos[i]);
 }
